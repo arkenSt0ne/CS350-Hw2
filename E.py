771f69a1459e873000ec90e7795bf15a92f5dec0
Ankit Kumar Singh <ankitsinghsjs@gmail.com>
2019-09-14 17:03:31 +0530

    Initial Commit

---
+++ E.py
@@ -0,0 +1,13 @@
+class E:
+    def __init__():
+        self.ds = dict()
+    def isBound( self, var_name ):
+        return var_name in self.ds.keys()
+    def adjunct( self, var_name, binding):
+        try:
+            self.ds[var_name] == binding
+            return 0
+        except :
+            return -1
+
+## add more operations on Environment!!
