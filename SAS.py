2ce6470bfbe55663a63ae5bc4b495afb6fc5d956
Ankit Kumar Singh <ankitsinghsjs@gmail.com>
2019-09-14 17:15:50 +0530

    Adding modules for SAS

---
+++ SAS.py
@@ -0,0 +1,9 @@
+class SAS:
+    def __init__():
+        self.__new_var_count__ = 0
+        self.__sas__ = None # Data Structure is not defined!. QUICK-UNION-FIND can be used.
+    def get_new_tmp():
+        t = self.__new_var_count__ 
+        self.__new_var_count__ += 1 
+        return '$var#'+str(t)
+    
